56ffd13e03d992675340790bffe15896628c7a72
Magnus Andersson <magnus08@gmail.com>
2017-10-13 15:38:18 +0200

    async experiment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {connect} from 'react-redux'
 
 class App extends Component {
   componentDidMount() {
-    this.props.fetchStuff(); // TODO: Hack
+    fetchStuff(); // TODO: Hack
   }
 
   render() {
@@ -26,39 +26,42 @@ class App extends Component {
 }
 
 
-const fetchStuff = () => {
-  const events = [
-    {
-      "id": 0,
-      "name": "A event",
-      "type": "item",
-      "energy": 42,
-      "user": 1
-    },
-    {
-      "id": 1,
-      "name": "Another event",
-      "type": "item",
-      "energy": 43,
-      "user": 1
-    },
-    {
-      "id": 2,
-      "name": "A bite for usr 2",
-      "type": "item",
-      "energy": 21,
-      "user": 2
-    }
-  ];
-  return (dispatch) => {
-    dispatch({
-      type: 'READING_EVENTS'
-    });
-    setTimeout(() => dispatch({
-      type: 'READ_EVENTS',
-      events
-    }), 2000);
-  }
+const fetchStuff = async () => {
+  let events = await fetch('http://localhost:6001/api/user/1/events');
+
+  console.log("Data = ", events);
+  // const events = [
+  //   {
+  //     "id": 0,
+  //     "name": "A event",
+  //     "type": "item",
+  //     "energy": 42,
+  //     "user": 1
+  //   },
+  //   {
+  //     "id": 1,
+  //     "name": "Another event",
+  //     "type": "item",
+  //     "energy": 43,
+  //     "user": 1
+  //   },
+  //   {
+  //     "id": 2,
+  //     "name": "A bite for usr 2",
+  //     "type": "item",
+  //     "energy": 21,
+  //     "user": 2
+  //   }
+  // ];
+  // return (dispatch) => {
+  //   dispatch({
+  //     type: 'READING_EVENTS'
+  //   });
+  //   setTimeout(() => dispatch({
+  //     type: 'READ_EVENTS',
+  //     events
+  //   }), 2000);
+  // }
 };
 const mapStateToProps = (state, ownProps) => (
   state // TODO: Just passing on state here, does not seem correct
@@ -67,6 +70,5 @@ const mapStateToProps = (state, ownProps) => (
 export default connect(
   mapStateToProps,
   {
-    fetchStuff: fetchStuff
   }
 )(App);
